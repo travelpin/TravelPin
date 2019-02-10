84dd5b1e3cf70e40b8098d30e08dd132bbac6317
sonedigo <sonedigo@gmail.com>
2019-02-10 15:54:20 -0600

    show how to push

---
+++ backend/src/rpc/searchInterests.java
@@ -0,0 +1,22 @@
+package rpc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "searchInterests")
+public class searchInterests extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        //List<?> interests = xx.searchinter(x,y,c)
+        JSONArray array = new JSONArray();
+    }
+}
