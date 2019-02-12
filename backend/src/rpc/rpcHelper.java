03d6e5cd8f5d528437b474c2ab0e522a62e91bc7
sonedigo <sonedigo@gmail.com>
2019-02-12 12:39:29 -0600

    completed rpcHelper.java

--- backend/src/rpc/rpcHelper.java
+++ backend/src/rpc/rpcHelper.java
@@ -1,7 +1,43 @@
 package rpc;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class rpcHelper {
+    public static void writeJsonArray(HttpServletResponse response, JSONArray array)throws IOException {
+        response.setContentType("application/json");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        PrintWriter out = response.getWriter();
+        out.print(array);
+        out.close();
+    }
+    public static void writeJsonObject(HttpServletResponse response, JSONObject obj)throws IOException{
+        response.setContentType("application/json");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        PrintWriter out = response.getWriter();
+        out.print(obj);
+        out.close();
+    }
 
+    public static JSONObject readJSONObject(HttpServletRequest request) {
+        StringBuilder sBuilder = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                sBuilder.append(line);
+            }
+            return new JSONObject(sBuilder.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new JSONObject();
+    }
 }
