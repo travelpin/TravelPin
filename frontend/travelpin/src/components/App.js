722f573d21c7c3391fc496a0e083c0ac39d9bf76
Alexander Yu <alexanderyu@Alexanders-MacBook-Pro.local>
2019-02-13 15:05:30 -0800

    Updated login without Rpc

---
+++ frontend/travelpin/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import {Main} from "./components/Main";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Main/>
+            </div>
+        );
+    }
+}
+
+export default App;
