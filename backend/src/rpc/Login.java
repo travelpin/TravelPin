7a4a964375b9237f5fe18b88ee571ab8bc3a86a4
Jingjia Yu <vauxmartin@gmail.com>
2019-02-14 14:05:27 -0800

    some try

--- backend/src/rpc/Login.java
+++ backend/src/rpc/Login.java
@@ -1,104 +1,13 @@
 package rpc;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+public class Login extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-import org.json.JSONObject;
-
-import db.DBConnection;
-import db.DBConnectionFactory;
-
-/**
- * Servlet implementation class Login
- */
-@WebServlet("/Login")
-public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DBConnection connection = DBConnectionFactory.getConnection();
-		try {
-			HttpSession session = request.getSession(false);
-			JSONObject obj = new JSONObject();
-			
-			if (session != null) {
-				String user_Id = session.getAttribute("user_id").toString();
-				obj.put("status", "OK").put("user_id", user_Id).put("name", connection.getFullname(user_Id));
-			} else {
-				response.setStatus(403);
-				obj.put("status", "Invalid Session");
-			}
-			//RpcHelper.writeJsonObject(response, obj);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			connection.close();
-		}
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DBConnection connection = DBConnectionFactory.getConnection();
-	
-		String user_Id = request.getParameter("user_id");
-		String password = request.getParameter("password");
-			
-		// Connect to mysql and verify username password
-			
-		try {
-				Class.forName("com.mysql.jdbc.Driver");
-				// loads driver
-				Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root"); // gets a new connection
-	 
-			
-				PreparedStatement ps = c.prepareStatement("select user_Id,password from users where user_Id=? and password=?");
-				ps.setString(1, user_Id);
-				ps.setString(2, password);
-	 
-				//Check if username and pw match
-				ResultSet rs = ps.executeQuery();
-	 
-				while (rs.next()) {
-					//Can be implemented in another way, for now just a success HTML returned
-					response.sendRedirect("success.html");
-					return;
-				}
-				response.sendRedirect("error.html");
-				return;
-			} catch (ClassNotFoundException | SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally{
-				connection.close();
-			}
-			
-		} 
-
-
-
+    }
 }
--- backend/src/rpc/Logout.java
+++ backend/src/rpc/Logout.java
@@ -1,46 +1,20 @@
 package rpc;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
-/**
- * Servlet implementation class Logout
- */
-@WebServlet("/Logout")
+@WebServlet(name = "Logout")
 public class Logout extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Logout() {
-        super();
-        // TODO Auto-generated constructor stub
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession(false);
-		if (session != null) {
-			session.invalidate();
-		}
-		//Can be implemented in another way. For now just return a logout HTMl page
-		response.sendRedirect("logOut.html");
-
-	}
-
+    }
 }
