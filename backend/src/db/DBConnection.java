374820a2e8aacd96b0fd256215034a1fec360371
Hanyu Lin <sonedigo@gmail.com>
2019-02-14 02:17:29 -0600

    Update DBConnection.java

--- backend/src/db/DBConnection.java
+++ backend/src/db/DBConnection.java
@@ -12,6 +12,111 @@ public interface DBConnection {
     public Set<Interest> getFavoriteInterests(String userId);
 
     public Set<String> getFavoriteInterestIds(String userId);
+    /**
+     * Close the connection.
+     */
+    public void close();
+
+    /**
+     * Insert the favorite items for a user.
+     *
+     * @param user_id
+     * @param liked_locations
+     */
+    public void setFavoriteInterests(String user_id, List<String> liked_locations);
+
+    /**
+     * Delete the favorite items for a user.
+     *
+     * @param user_id
+     * @param liked_locations
+     */
+    public void unsetFavoriteInterests(String user_id, List<String> liked_locations);
+
+
+    /**
+     * Insert the favorite items for a user.
+     *
+     * @param user_id
+     * @param liked_locations
+     */
+    public void setFavoritePlans(String user_id, List<String> liked_locations);
+
+    /**
+     * Delete the favorite items for a user.
+     *
+     * @param user_id
+     * @param liked_locations
+     */
+    public void unsetFavoritePlans(String user_id, List<String> liked_locations);
+
+
+    /**
+     * Get the favorite item id for a user.
+     *
+     * @param user_id
+     * @return itemIds
+     */
+    public Set<String> getFavoriteInterestIds(String user_id);
+
+    /**
+     * Get the favorite items for a user.
+     *
+     * @param userId
+     * @return items
+     */
+
+
+
+
+//    public Set<Item> getFavoriteItems(String userId);
+//
+//    /**
+//     * Gets categories based on item id
+//     *
+//     * @param itemId
+//     * @return set of categories
+//     */
+//    public Set<String> getCategories(String itemId);
+//
+//    /**
+//     * Search items near a geolocation and a term (optional).
+//     *
+//     * @param userId
+//     * @param lat
+//     * @param lon
+//     * @param term
+//     *            (Nullable)
+//     * @return list of items
+//     */
+//    public List<Item> searchItems(double lat, double lon, String term);
+//
+//    /**
+//     * Save item into db.
+//     *
+//     * @param item
+//     */
+//    public void saveItem(Item item);
+//
+//    /**
+//     * Get full name of a user. (This is not needed for main course, just for demo
+//     * and extension).
+//     *
+//     * @param userId
+//     * @return full name of the user
+//     */
+//    public String getFullname(String userId);
+
+
+    /**
+     * Return whether the credential is correct. (This is not needed for main
+     * course, just for demo and extension)
+     *
+     * @param userId
+     * @param password
+     * @return boolean
+     */
+    public boolean verifyLogin(String userId, String password);
 
 
 }
