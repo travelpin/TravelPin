ebb63c81e06e1222ac8de1f733269a87e7fd3ae1
Miao Xiaoyang <xiaoyanm@usc.edu>
2019-02-18 10:36:32 -0800

    change icon name

--- frontend/travelpin/src/components/TopBar.js
+++ frontend/travelpin/src/components/TopBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import map from "../asserts/images/map.svg";
+import mapIcon from "../asserts/images/mapIcon.svg";
 
 export class TopBar extends React.Component {
     render() {
         return(
             <div className="logo-name">
-                <img src={map} className="top-logo" alt="logo" />
+                <img src={mapIcon} className="top-logo" alt="logo" />
                 <span className="logo-name">TravelPin</span>
             </div>
         );
