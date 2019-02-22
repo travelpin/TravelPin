095032d2b8faf5c389496f460005bc83684d031a
sonedigo <sonedigo@gmail.com>
2019-02-22 15:08:28 -0600

    update google map component with react-google-maps

--- frontend/travelpin/src/components/GoogleMap.js
+++ frontend/travelpin/src/components/GoogleMap.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import {API_KEY} from '../constants';
+import {GOOGLE_MAP_API} from "../secureConstants";
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export class SimpleMap extends Component {
+class NormalGoogleMap extends Component {
     static defaultProps = {
         center: {
             lat: 40.71,
@@ -17,18 +18,25 @@ export class SimpleMap extends Component {
         return (
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: API_KEY + "T34rUkk".substring(0, 5) }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                {/*<GoogleMapReact*/}
+                    {/*bootstrapURLKeys={{ key: API_KEY + "T34rUkk".substring(0, 5) }}*/}
+                    {/*defaultCenter={this.props.center}*/}
+                    {/*defaultZoom={this.props.zoom}*/}
+                {/*>*/}
+                    {/*<AnyReactComponent*/}
+                        {/*lat={59.955413}*/}
+                        {/*lng={30.337844}*/}
+                        {/*text={'Kreyser Avrora'}*/}
+                    {/*/>*/}
+                {/*</GoogleMapReact>*/}
+                <GoogleMap
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
                 >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text={'Kreyser Avrora'}
-                    />
-                </GoogleMapReact>
+                </GoogleMap>
             </div>
         );
     }
 }
+
+export const TravelPinGoogleMap = withScriptjs(withGoogleMap(NormalGoogleMap));
