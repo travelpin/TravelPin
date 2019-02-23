7a3aba2589f36296901ac791fde564f93b767b89
sonedigo <sonedigo@gmail.com>
2019-02-23 14:00:02 -0600

    relocate API_KEY, API_ROOT

--- frontend/travelpin/src/components/GoogleMap.js
+++ frontend/travelpin/src/components/GoogleMap.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
-import {API_KEY} from '../constants';
 import {GOOGLE_MAP_API} from "../secureConstants";
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class NormalGoogleMap extends Component {
     static defaultProps = {
@@ -18,17 +15,6 @@ class NormalGoogleMap extends Component {
         return (
             // Important! Always set the container height explicitly
             <div style={{ height: '100vh', width: '100%' }}>
-                {/*<GoogleMapReact*/}
-                    {/*bootstrapURLKeys={{ key: API_KEY + "T34rUkk".substring(0, 5) }}*/}
-                    {/*defaultCenter={this.props.center}*/}
-                    {/*defaultZoom={this.props.zoom}*/}
-                {/*>*/}
-                    {/*<AnyReactComponent*/}
-                        {/*lat={59.955413}*/}
-                        {/*lng={30.337844}*/}
-                        {/*text={'Kreyser Avrora'}*/}
-                    {/*/>*/}
-                {/*</GoogleMapReact>*/}
                 <GoogleMap
                 defaultCenter={this.props.center}
                 defaultZoom={this.props.zoom}
